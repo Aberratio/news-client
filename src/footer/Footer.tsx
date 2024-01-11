fef79a4b9d078da7428488ff735cddca3c5fcfd9
Aberratio <joannakokot97@gmail.com>
2024-01-11 20:35:12 +0100

    refactor footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "layout/components/typography/Typography";
 import styled from "styled-components";
+import { WebcoBar } from "./WebcoBar";
+import { DescriptionColumn } from "./DescriptionColumn";
+import { ElementType, InfoColumn } from "./InfoColumn";
 
 export const Footer = () => {
   return (
@@ -7,130 +9,16 @@ export const Footer = () => {
       <Wrapper>
         <Container>
           <Row>
-            <FirstColumn>
-              <LogoWrapper href="#">
-                <Logo />
-              </LogoWrapper>
-              <DescriptionContainer>
-                <StyledTypography color="white" wordBreak="break-word">
-                  "Głos Milicza" to wychodzący od 1995 roku tygodnik lokalny,
-                  którego misją jest dostarczanie mieszkańcom gmin: Milicz,
-                  Krośnice, Cieszków najświeższych wiadomości oraz ciekawych
-                  artykułów dotyczących różnych dziedzin życia w naszej lokalnej
-                  społeczności.
-                </StyledTypography>
-                <StyledTypography color="white" wordBreak="break-word">
-                  Tydzień w tydzień nasi dziennikarze z pasją, zaangażowaniem i
-                  dociekliwością zbierają informacje z całego powiatu
-                  milickiego, by podzielić się nimi z Czytelnikami.
-                </StyledTypography>
-                <StyledTypography color="white" wordBreak="break-word">
-                  Staramy się słuchać i szybko reagować na potrzeby naszych
-                  Czytelników. To z myślą o nich powstał portal glosmilicza.pl.
-                </StyledTypography>
-              </DescriptionContainer>
-            </FirstColumn>
-            <SecondColumn>
-              <HeaderWrapper>
-                <Typography variant="h3" color="rgb(46,104,150)">
-                  Ważne informacje
-                </Typography>
-              </HeaderWrapper>
-              <LinksList>
-                <ListElement>
-                  <Link href="#">
-                    <Typography>Regulamin</Typography>
-                  </Link>
-                </ListElement>
-              </LinksList>
-            </SecondColumn>
-            <SecondColumn>
-              <HeaderWrapper>
-                <Typography variant="h3" color="rgb(46,104,150)">
-                  Redakcja Głosu Milicza
-                </Typography>
-              </HeaderWrapper>
-              <LinksList>
-                <ListElement>
-                  <Link href="tel:713830021">
-                    <Typography>71-3830-021</Typography>
-                  </Link>
-                  <Link href="tel:713831189">
-                    <Typography>71-3831-189</Typography>
-                  </Link>
-                  <Link href="mailto:sekretariat@glosmilicza.pl">
-                    <Typography>sekretariat@glosmilicza.pl</Typography>
-                  </Link>
-                  <Link href="mailto:gmmilicz@pro.onet.pl">
-                    <Typography>gmmilicz@pro.onet.pl</Typography>
-                  </Link>
-                </ListElement>
-                <ListElement>
-                  <Typography>ul. T. Kościuszki 22</Typography>
-                  <Typography>56-300 Milicz</Typography>
-                </ListElement>
-                <ListElement>
-                  <Typography>
-                    <strong>NIP</strong>: 916-10-01-943
-                  </Typography>
-                  <Typography>
-                    <strong>REGON</strong>: 930445239
-                  </Typography>
-                </ListElement>
-              </LinksList>
-            </SecondColumn>
+            <DescriptionColumn />
+            <InfoColumn {...secondColumnMock} />
+            <InfoColumn {...thirdColumnMock} />
           </Row>
         </Container>
       </Wrapper>
-      <WebcoBar>
-        <WebcoBarContainer>
-          <Typography>Copyright © 2021-2024</Typography>
-          <WebcoBarLink href="https:webcodesign.pl" target="_blank">
-            <Typography> webco.design </Typography>
-          </WebcoBarLink>
-          <Typography>☕ All rights reserved.</Typography>
-        </WebcoBarContainer>
-      </WebcoBar>
+      <WebcoBar />
     </footer>
   );
 };
-
-const StyledTypography = styled(Typography)`
-  text-align: left !important;
-`;
-
-const DescriptionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-  text-align: left;
-`;
-
-const WebcoBarLink = styled.a`
-  margin: 0 8px;
-  color: green;
-  cursor: pointer;
-`;
-
-const WebcoBarContainer = styled.div`
-  margin-right: auto;
-  margin-left: auto;
-  padding-right: 15px;
-  padding-left: 15px;
-  width: 100%;
-  padding-top: 15px;
-  padding-bottom: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 1080px;
-  min-height: 80px;
-  color: white;
-`;
-
-const WebcoBar = styled.div`
-  background-color: #151515;
-`;
 
 const Wrapper = styled.div`
   padding-top: 40px;
@@ -151,66 +39,72 @@ const Row = styled.div`
   gap: 32px;
 `;
 
-const FirstColumn = styled.div`
-  width: 100%;
-  min-height: 1px;
-  padding-right: 15px;
-  padding-left: 15px;
-  padding-bottom: 20px;
-`;
+const secondColumnMock = {
+  header: "Ważne informacje",
+  elements: [
+    {
+      id: 1,
+      type: ElementType.Link,
+      content: [
+        {
+          text: "Regulamin",
+          link: "#",
+        },
+      ],
+    },
+  ],
+};
 
-const LogoWrapper = styled.a`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  height: 95px;
-`;
-
-const Logo = styled.img`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  height: 95px;
-`;
-
-const SecondColumn = styled.div`
-  width: 100%;
-  min-height: 1px;
-  padding-right: 15px;
-  padding-left: 15px;
-  padding-bottom: 20px;
-`;
-
-const HeaderWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  height: 95px;
-`;
-
-const LinksList = styled.ul`
-  margin: 0;
-  list-style-type: none;
-  margin-top: -12px;
-  color: white;
-`;
-
-const ListElement = styled.li`
-  margin: 0;
-  list-style-type: none;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 5px;
-  padding-right: 5px;
-  border: none;
-`;
-
-const Link = styled.a`
-  text-decoration: none;
-  background-color: transparent;
-  touch-action: manipulation;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  transition: all 0.3s;
-  cursor: pointer;
-`;
+const thirdColumnMock = {
+  header: "Redakcja Głosu Milicza",
+  elements: [
+    {
+      id: 1,
+      type: ElementType.Link,
+      content: [
+        {
+          text: "71-3830-021",
+          link: "tel:713830021",
+        },
+        {
+          text: "71-3831-189",
+          link: "tel:713831189",
+        },
+        {
+          text: "sekretariat@glosmilicza.pl",
+          link: "mailto:sekretariat@glosmilicza.pl",
+        },
+        {
+          text: "gmmilicz@pro.onet.pl",
+          link: "mailto:gmmilicz@pro.onet.pl",
+        },
+      ],
+    },
+    {
+      id: 2,
+      type: ElementType.Text,
+      content: [
+        {
+          text: "ul. T. Kościuszki 22",
+        },
+        {
+          text: "56-300 Milicz",
+        },
+      ],
+    },
+    {
+      id: 3,
+      type: ElementType.BoldedText,
+      content: [
+        {
+          bolded: "NIP",
+          text: "916-10-01-943",
+        },
+        {
+          bolded: "REGON",
+          text: "930445239",
+        },
+      ],
+    },
+  ],
+};
