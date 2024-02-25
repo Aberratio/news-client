66edd6f1c67131ebc277a3eb33035dc4b3892bae
Aberratio <joannakokot97@gmail.com>
2024-02-25 21:06:57 +0100

    add OPTIONS to api

--- src/app/api/articles/stats/route.ts
+++ src/app/api/articles/stats/route.ts
@@ -1,4 +1,4 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { query } from "core/db/query";
 
 export const GET = async (request: NextRequest) => {
@@ -16,4 +16,20 @@ export const GET = async (request: NextRequest) => {
     console.error("Error fetching article stats:", error);
     return Response.json([], { status: 500 });
   }
+};
+
+export const OPTIONS = (request: Request) => {
+  const allowedOrigin = request.headers.get("origin");
+  const response = new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": allowedOrigin || "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+
+  return response;
 };
--- src/app/api/visits/route.ts
+++ src/app/api/visits/route.ts
@@ -1,4 +1,5 @@
 import { query } from "core/db/query";
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
@@ -35,4 +36,20 @@ export const PUT = async (request: Request) => {
       { status: 500 }
     );
   }
+};
+
+export const OPTIONS = (request: Request) => {
+  const allowedOrigin = request.headers.get("origin");
+  const response = new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": allowedOrigin || "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+
+  return response;
 };
