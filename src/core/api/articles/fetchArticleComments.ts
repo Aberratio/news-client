35328f4c6e4646e40f0e0655c5ea1e9d8adf6621
Aberratio <joannakokot97@gmail.com>
2024-02-27 21:31:41 +0100

    add headers with origin to prevent cors

--- src/core/api/articles/fetchArticleComments.ts
+++ src/core/api/articles/fetchArticleComments.ts
@@ -6,6 +6,14 @@ export const fetchArticleComments = async (_id: string) => {
   return fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/articles/comments`, {
     method: "POST",
     body: JSON.stringify({ _id }),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
     next: {
       revalidate: 360,
       tags: ["comments"],
