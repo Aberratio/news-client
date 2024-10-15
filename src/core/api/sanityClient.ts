d46c4c98001629c8f135d8c729350b7b44274bb4
Aberratio <joannakokot97@gmail.com>
2024-10-15 20:53:26 +0200

    change sanity client version

--- src/core/api/sanityClient.ts
+++ src/core/api/sanityClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
-  apiVersion: "2022-03-07",
+  apiVersion: "v2022-03-07",
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
 });
